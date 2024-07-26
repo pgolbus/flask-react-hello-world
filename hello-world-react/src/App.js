2a527adbda6f367e81d55dc436d24078dd7687e6
Peter B. Golbus <pgolbus@gmail.com>
2024-07-25 20:04:56 -0400

    that worked a minute ago

--- hello-world-react/src/App.js
+++ hello-world-react/src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 
 const apiUrl = process.env.REACT_APP_FLASK_API;
+console.log("apiUrl: ", apiUrl);
 
 function App() {
   const [message, setMessage] = useState('');
